7a49e9348e2096dc81cb9e2eb50dcd60208a7513
Dagaet <alojera15@gmail.com>
2024-04-21 19:14:28 +0100

    feat: created table to set up the data

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -9,14 +9,38 @@ function Home() {
         <h2>admin dashboard</h2>
         <hr />
         <div className={style.buttonContainer}>
-          <button>Principal Page</button>
-          <button>Consult Users</button>
-          <button>Create User</button>
+          <button className={style.buttonOption}>Principal Page</button>
+          <button className={style.buttonOption}>Consult Users</button>
+          <button className={style.buttonOption}>Create User</button>
         </div>
       </div>
-      <div>
-        <div>ALgo</div>
-        <div>Algo</div>
+      <div className={style.userMainContainer}>
+        <div className={style.userInfoContainer}>ALgo</div>
+        <div className={style.userDataContainer}>
+          <table>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>ID</th>
+              <th>Department</th>
+              <th>Creation Date</th>
+            </tr>
+            <tr>
+              <td>#1</td>
+              <td>Joselito Pérez Almenara</td>
+              <td>123456789</td>
+              <td>IT</td>
+              <td>13/02/9999</td>
+            </tr>
+            <tr>
+              <td>#2</td>
+              <td>Alghien</td>
+              <td>someones number</td>
+              <td>some dep</td>
+              <td>13/02/9999</td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
   );
