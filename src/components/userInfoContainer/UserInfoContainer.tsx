54c28caa89f7e58aea9c38b949488a49b1d4f43f
Dagaet <alojera15@gmail.com>
2024-04-29 15:05:42 +0100

    refactor: created component for userInfoContainer

---
+++ src/components/userInfoContainer/UserInfoContainer.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { IoMdArrowDropdown } from "react-icons/io";
+import style from "./UserInfoContainer.module.css";
+import { LiaUserPlusSolid } from "react-icons/lia";
+import { FaCommentDots } from "react-icons/fa6";
+import { FaUser, FaUserCheck } from "react-icons/fa";
+
+function UserInfoContainer() {
+  return (
+    <div className={style.userInfoContainer}>
+      <div className={style.userInfoSubContainers}>
+        <div className={style.subContainers}>
+          <div className={style.usersContainer}>
+            <div className={style.usersData}>
+              <h4 className={style.usersText}>New Users this week</h4>
+              <h4 className={style.usersQuantity}>2034</h4>
+            </div>
+            <div>
+              <LiaUserPlusSolid size={90} />
+            </div>
+          </div>
+        </div>
+        <div className={style.subContainers}>
+          <div className={style.usersContainer}>
+            <div className={style.usersData}>
+              <h4 className={style.usersText}>All Messages</h4>
+              <h4 className={style.usersQuantity}>33120</h4>
+            </div>
+            <div>
+              <FaCommentDots size={90} style={{ color: "white" }} />
+            </div>
+          </div>
+        </div>
+        <div className={style.subContainers}>
+          <div className={style.usersContainer}>
+            <div className={style.usersData}>
+              <h4 className={style.usersText}>Total Users</h4>
+              <h4 className={style.usersQuantity}>4056</h4>
+            </div>
+            <div>
+              <FaUser size={90} style={{ color: "white" }} />
+            </div>
+          </div>
+        </div>
+        <div className={style.subContainers}>
+          <div className={style.usersContainer}>
+            <div className={style.usersData}>
+              <h4 className={style.usersText}>Active Users</h4>
+              <h4 className={style.usersQuantity}>305</h4>
+            </div>
+            <div>
+              <FaUserCheck size={90} style={{ color: "white" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={style.adminUserDropdown}>
+        Admin user <IoMdArrowDropdown size={45} />
+      </div>
+    </div>
+  );
+}
+
+export default UserInfoContainer;
--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { IoMdArrowDropdown } from "react-icons/io";
 import style from "./Home.module.css";
-import { LiaUserPlusSolid } from "react-icons/lia";
-import { FaCommentDots } from "react-icons/fa6";
-import { FaUser, FaUserCheck } from "react-icons/fa";
 import OptionContainer from "../../components/optionContainer/OptionContainer";
+import UserInfoContainer from "../../components/userInfoContainer/UserInfoContainer";
 
 /**
  * This function creates the main page with some mocked data
@@ -13,69 +10,9 @@ import OptionContainer from "../../components/optionContainer/OptionContainer";
 function Home() {
   return (
     <div className={style.mainContainer}>
-      {/* <div className={style.optionContainer}>
-        <h2>ChatXpress </h2>
-        <h2>admin dashboard</h2>
-        <hr />
-        <div className={style.buttonContainer}>
-          <button className={style.buttonOption}>Principal Page</button>
-          <button className={style.buttonOption}>Consult Users</button>
-          <button className={style.buttonOption}>Create User</button>
-        </div>
-      </div> */}
       <OptionContainer />
       <div className={style.userMainContainer}>
-        <div className={style.userInfoContainer}>
-          <div className={style.userInfoSubContainers}>
-            <div className={style.subContainers}>
-              <div className={style.usersContainer}>
-                <div className={style.usersData}>
-                  <h4 className={style.usersText}>New Users this week</h4>
-                  <h4 className={style.usersQuantity}>2034</h4>
-                </div>
-                <div>
-                  <LiaUserPlusSolid size={90} />
-                </div>
-              </div>
-            </div>
-            <div className={style.subContainers}>
-              <div className={style.usersContainer}>
-                <div className={style.usersData}>
-                  <h4 className={style.usersText}>All Messages</h4>
-                  <h4 className={style.usersQuantity}>33120</h4>
-                </div>
-                <div>
-                  <FaCommentDots size={90} style={{ color: "white" }} />
-                </div>
-              </div>
-            </div>
-            <div className={style.subContainers}>
-              <div className={style.usersContainer}>
-                <div className={style.usersData}>
-                  <h4 className={style.usersText}>Total Users</h4>
-                  <h4 className={style.usersQuantity}>4056</h4>
-                </div>
-                <div>
-                  <FaUser size={90} style={{ color: "white" }} />
-                </div>
-              </div>
-            </div>
-            <div className={style.subContainers}>
-              <div className={style.usersContainer}>
-                <div className={style.usersData}>
-                  <h4 className={style.usersText}>Active Users</h4>
-                  <h4 className={style.usersQuantity}>305</h4>
-                </div>
-                <div>
-                  <FaUserCheck size={90} style={{ color: "white" }} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={style.adminUserDropdown}>
-            Admin user <IoMdArrowDropdown size={45} />
-          </div>
-        </div>
+        <UserInfoContainer />
         <div className={style.userDataContainer}>
           <table>
             <tr>
