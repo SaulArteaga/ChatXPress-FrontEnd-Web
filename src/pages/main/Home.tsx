865ea03b6d20ae1cf3f2778471859d94f58908a8
Dagaet <alojera15@gmail.com>
2024-04-29 15:14:39 +0100

    refactor: created component for userDataContainer

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./Home.module.css";
 import OptionContainer from "../../components/optionContainer/OptionContainer";
 import UserInfoContainer from "../../components/userInfoContainer/UserInfoContainer";
+import UserDataContainer from "../../components/userDataContainer/UserDataContainer";
 
 /**
  * This function creates the main page with some mocked data
@@ -13,31 +14,7 @@ function Home() {
       <OptionContainer />
       <div className={style.userMainContainer}>
         <UserInfoContainer />
-        <div className={style.userDataContainer}>
-          <table>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>ID</th>
-              <th>Department</th>
-              <th>Creation Date</th>
-            </tr>
-            <tr>
-              <td>#1</td>
-              <td>Joselito Pérez Almenara</td>
-              <td>123456789</td>
-              <td>IT</td>
-              <td>13/02/9999</td>
-            </tr>
-            <tr>
-              <td>#2</td>
-              <td>Alghien</td>
-              <td>someones number</td>
-              <td>some dep</td>
-              <td>13/02/9999</td>
-            </tr>
-          </table>
-        </div>
+        <UserDataContainer />
       </div>
     </div>
   );
