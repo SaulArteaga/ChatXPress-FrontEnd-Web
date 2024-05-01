d744da7aaa0149e7347e063c8eca1bc0b825ee22
Dagaet <alojera15@gmail.com>
2024-05-01 15:52:24 +0100

    feat(FindUserContainer): created functions to handle the api call

--- src/components/findUser/FindUserContainer.tsx
+++ src/components/findUser/FindUserContainer.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import style from "./FindUserContainer.module.css";
+import { getUserByEmail } from "../../services/users.services";
+import React from "react";
+import { usersContext } from "../../contexts/usersContext";
 
 function FindUserContainer() {
+  const [email, setEmail] = useState("");
+  const { setUsers } = React.useContext(usersContext);
+
+  const findUser = async () => {
+    const data = await getUserByEmail(email);
+    console.log(data);
+    setUsers(data);
+  };
+
+  const handleInputChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div className={style.findUserContainer}>
       <div className={style.findUserSubContainers}>
@@ -10,8 +26,15 @@ function FindUserContainer() {
         </div>
         <div className={style.subContainer}>
           <div className={style.findInputContainer}>
-            <input className={style.customInput} placeholder="ID"></input>
-            <button className={style.buttonStyle}>FIND</button>
+            <input
+              className={style.customInput}
+              placeholder="EMAIL"
+              value={email}
+              onChange={handleInputChange}
+            ></input>
+            <button className={style.buttonStyle} onClick={() => findUser()}>
+              FIND
+            </button>
           </div>
         </div>
       </div>
