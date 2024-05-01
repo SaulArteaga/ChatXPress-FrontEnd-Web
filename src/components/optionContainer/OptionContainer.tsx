a6209fdc0f9cae89599c608050d5c605a2ecbaee
Dagaet <alojera15@gmail.com>
2024-05-01 14:39:17 +0100

    feat(OptionContainer): created onClick options for the buttons

--- src/components/optionContainer/OptionContainer.tsx
+++ src/components/optionContainer/OptionContainer.tsx
@@ -1,15 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import style from "./OptionContainer.module.css";
 
 function OptionContainer() {
+  const navigate = useNavigate();
+
   return (
     <div className={style.optionContainer}>
       <h2>ChatXpress </h2>
       <h2>admin dashboard</h2>
       <hr />
       <div className={style.buttonContainer}>
-        <button className={style.buttonOption}>Principal Page</button>
-        <button className={style.buttonOption}>Consult Users</button>
-        <button className={style.buttonOption}>Create User</button>
+        <button
+          className={style.buttonOption}
+          onClick={() => navigate("/home")}
+        >
+          Principal Page
+        </button>
+        <button
+          className={style.buttonOption}
+          onClick={() => navigate("/findUser")}
+        >
+          Consult Users
+        </button>
+        <button
+          className={style.buttonOption}
+          onClick={() => navigate("/createUser")}
+        >
+          Create User
+        </button>
       </div>
     </div>
   );
