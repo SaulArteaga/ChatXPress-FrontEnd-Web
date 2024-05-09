0a0e0f6610bc8442070f154ab923b48c6883e586
Dagaet <alojera15@gmail.com>
2024-05-09 13:45:33 +0100

    feat: created dropdown menu styles & functionality

--- src/components/userDropdownContainer/UserDropdownContainer.tsx
+++ src/components/userDropdownContainer/UserDropdownContainer.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import style from "./UserDropdownContainer.module.css";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { userLoggedContext } from "../../contexts/isUserLoggedContext";
+import { useNavigate } from "react-router";
 
 function UserDropdownComponent() {
+  const navigate = useNavigate();
+  const userData = React.useContext(userLoggedContext);
+
+  const handleLogout = () => {
+    localStorage.removeItem("JWT");
+    navigate("/");
+  };
+
   return (
     <div className={style.adminUserDropdown}>
-      Admin user <IoMdArrowDropdown size={45} />
+      <div className={style.dropdown}>
+        <div className={style.infoUser}>
+          {userData.name} Logged <IoMdArrowDropdown size={45} />
+        </div>
+        <div className={style.content}>
+          <div className={style.dropdownInfo} onClick={handleLogout}>
+            Logout
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
