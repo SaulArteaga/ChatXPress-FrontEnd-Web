8a56564d89da902d396ff94b89d98235caa1cd1e
Dagaet <alojera15@gmail.com>
2024-05-22 10:04:31 +0100

    fix: now the create user asks before creating user

--- src/components/createUserContainer/CreateUserContainer.tsx
+++ src/components/createUserContainer/CreateUserContainer.tsx
@@ -11,19 +11,33 @@ import { useNavigate } from "react-router";
 function CreateUserContainer() {
   const navigate = useNavigate();
 
-  const [userModified, setUserModified] = useState<IUsersRequest>({});
+  const initUser: IUsersRequest = {
+    name: "",
+    department: "",
+    email: "",
+    password: "",
+    lastname: "",
+  };
+
+  const [userCreated, setUserModified] = useState<IUsersRequest>(initUser);
 
   const handleInputChange = (e: any) => {
     setUserModified({
-      ...userModified,
+      ...userCreated,
       [e.target.name]: e.target.value,
     });
   };
 
   const createUserHandler = async () => {
-    console.log(userModified);
-    await createUser(userModified);
-    navigate("/home");
+    const confirmChoice = window.confirm("Do you want to create the user?");
+    console.log(userCreated);
+    if (confirmChoice) {
+      await createUser(userCreated);
+      navigate("/home");
+    } else {
+      window.alert("Canceling operation");
+      setUserModified(initUser);
+    }
   };
 
   return (
@@ -49,33 +63,33 @@ function CreateUserContainer() {
                 name="name"
                 className={style.inputData}
                 onChange={handleInputChange}
-                value={userModified.name}
+                value={userCreated.name}
               ></input>
               <input
                 id="lastname"
                 name="lastname"
                 className={style.inputData}
                 onChange={handleInputChange}
-                value={userModified.lastname}
+                value={userCreated.lastname}
               ></input>
               <input
                 id="email"
                 name="email"
                 className={style.inputData}
-                value={userModified.email}
+                value={userCreated.email}
                 onChange={handleInputChange}
               ></input>
               <input
                 id="department"
                 name="department"
                 className={style.inputData}
-                value={userModified.department}
+                value={userCreated.department}
                 onChange={handleInputChange}
               ></input>
               <input
                 className={style.inputData}
                 name="password"
-                value={userModified.password}
+                value={userCreated.password}
                 onChange={handleInputChange}
               ></input>
             </div>
