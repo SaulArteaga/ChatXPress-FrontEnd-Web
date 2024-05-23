06b8b0faf055a70a30eeff34ef652d2486b166dd
Dagaet <alojera15@gmail.com>
2024-05-23 12:47:32 +0100

    fix: password now is not shown when creating

--- src/components/createUserContainer/CreateUserContainer.tsx
+++ src/components/createUserContainer/CreateUserContainer.tsx
@@ -115,6 +115,7 @@ function CreateUserContainer() {
                 className={style.inputData}
                 id="password"
                 name="password"
+                type="password"
                 value={userCreated.password}
                 onChange={handleInputChange}
               ></input>
